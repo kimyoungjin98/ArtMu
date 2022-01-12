baabb70332152fa70e82a0f5da269068b6f5427c
kimyoungjin98 <gyu250@naver.com>
2022-01-12 18:19:49 +0900

    시작

---
+++ src/main/java/com/malcom/malang/MalangApplication.java
@@ -0,0 +1,13 @@
+package com.malcom.malang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malcom/malang/MalangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcom.malang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
