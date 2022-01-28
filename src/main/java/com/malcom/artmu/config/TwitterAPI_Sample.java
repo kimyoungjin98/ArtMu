ef718a3f43e3c75d1a548f5d3d7a1b9e359298d1
Nanask <ybhag1005@daum.net>
2022-01-28 21:49:02 +0900

    API

---
+++ src/main/java/com/malcom/artmu/config/TwitterAPI_Sample.java
@@ -0,0 +1,8 @@
+package com.malcom.artmu.config;
+
+public class TwitterAPI_Sample {
+
+    public static final String CLIENT_ID = "YOUR TWITTER CLIENT ID";
+    public static final String CLIENT_SECRET = "YOUR TWITTER CLIENT SECRET";
+    public static final String BEARER_TOKEN = "GET/2/tweets/:id";
+}
