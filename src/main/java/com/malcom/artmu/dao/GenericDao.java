a93854bcdb517a0c90d9fc4e0c0854be967bbfd1
soyeon-noh <1010qwerty12@gmail.com>
2022-02-06 19:34:03 +0900

    트위터 데이터 가져오기 테스트

--- src/main/java/com/malcom/artmu/dao/GenericDao.java
+++ src/main/java/com/malcom/artmu/dao/GenericDao.java
@@ -1,12 +1,10 @@
 package com.malcom.artmu.dao;
 
-import com.malcom.artmu.model.Data;
-
 import java.util.List;
 
 public interface GenericDao<VO, PK> {
 
-    public Data getData();
-    public List<Data> getList();
+    public VO getData();
+    public List<VO> getList();
 
 }
--- src/main/java/com/malcom/artmu/service/TwitterLoadingService.java
+++ src/main/java/com/malcom/artmu/service/TwitterLoadingService.java
@@ -2,10 +2,16 @@ package com.malcom.artmu.service;
 
 import com.malcom.artmu.config.TwitterAPI;
 import io.github.redouane59.twitter.TwitterClient;
+import io.github.redouane59.twitter.dto.endpoints.AdditionalParameters;
+import io.github.redouane59.twitter.dto.tweet.TweetList;
+import io.github.redouane59.twitter.dto.tweet.TweetV2;
+import io.github.redouane59.twitter.dto.user.UserV2;
 import io.github.redouane59.twitter.signature.TwitterCredentials;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Service
 public class TwitterLoadingService {
 
     // 이 객체를 통해 트위터와 통신
@@ -17,6 +23,28 @@ public class TwitterLoadingService {
             .build());
 
     public void TestTwitterLoading() {
+        LocalDateTime endLocalDateTime = LocalDateTime.now();
+        LocalDateTime startLocalDateTime = endLocalDateTime.minusDays(7);
+        // 파라메터 설정
+        AdditionalParameters additionalParameters = AdditionalParameters.builder()
+                .startTime(startLocalDateTime)
+                .endTime(endLocalDateTime)
+                .build();
+
+        UserV2 userV2 = twitterClient.getUserFromUserName("@빼고 닉네임");
+        System.out.println("아이디 로딩 체크");
+        System.out.println("아이디 : " + userV2.getId());
+
+        TweetList tweetList =
+                twitterClient.getUserTimeline(userV2.getId(), additionalParameters);
+        System.out.println("트윗 로딩 체크");
+        System.out.println("가져온 트윗 수  : " + tweetList.getData().size() );
+        for(TweetV2.TweetData tweet : tweetList.getData()){
+            System.out.println("Id : " + tweet.getId());
+            System.out.println("text : " + tweet.getText());
+            System.out.println("=".repeat(10));
+        }
+
 
     }
 }
