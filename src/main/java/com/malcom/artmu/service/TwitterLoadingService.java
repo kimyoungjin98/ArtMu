4f449584a38490daf50bc71805e1290db4ec57cc
soyeon-noh <1010qwerty12@gmail.com>
2022-02-08 23:54:33 +0900

    인증오류

--- src/main/java/com/malcom/artmu/service/TwitterLoadingService.java
+++ src/main/java/com/malcom/artmu/service/TwitterLoadingService.java
@@ -29,26 +29,27 @@ public class TwitterLoadingService {
         AdditionalParameters additionalParameters = AdditionalParameters.builder()
                 .startTime(startLocalDateTime)
                 .endTime(endLocalDateTime)
+                .maxResults(10)
                 .build();
 
         /** 특정 유저의 트윗 가져오기*/
-        UserV2 userV2 = twitterClient.getUserFromUserName("@빼고 닉네임");
-        System.out.println("아이디 로딩 체크");
-        System.out.println("아이디 : " + userV2.getId());
-
-        TweetList tweetList =
-                twitterClient.getUserTimeline(userV2.getId(), additionalParameters);
-        System.out.println("트윗 로딩 체크");
-        System.out.println("가져온 트윗 수  : " + tweetList.getData().size() );
-        for(TweetV2.TweetData tweet : tweetList.getData()){
-            System.out.println("Id : " + tweet.getId());
-            System.out.println("text : " + tweet.getText());
-            System.out.println("=".repeat(10));
-        }
+//        UserV2 userV2 = twitterClient.getUserFromUserName("@뺀 닉네임");
+//        System.out.println("아이디 로딩 체크");
+//        System.out.println("아이디 : " + userV2.getId());
+//
+//        TweetList tweetList =
+//                twitterClient.getUserTimeline(userV2.getId(), additionalParameters);
+//        System.out.println("트윗 로딩 체크");
+//        System.out.println("가져온 트윗 수  : " + tweetList.getData().size() );
+//        for(TweetV2.TweetData tweet : tweetList.getData()){
+//            System.out.println("Id : " + tweet.getId());
+//            System.out.println("text : " + tweet.getText());
+//            System.out.println("=".repeat(10));
+//        }
 
         /** 검색 */
-        String query = "#커미션";
-        TweetList tweetList2 = twitterClient.searchAllTweets(query);
+        String query = "커미션";
+        TweetList tweetList2 = twitterClient.searchAllTweets(query, additionalParameters);
         System.out.println("트윗 로딩 체크2");
         System.out.println("가져온 트윗 수2  : " + tweetList2.getData().size() );
         for(TweetV2.TweetData tweet : tweetList2.getData()){
